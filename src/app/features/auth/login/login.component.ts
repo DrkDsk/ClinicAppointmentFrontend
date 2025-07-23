11bc88ab6723d0d7c1057d6ea33f20f4cf36fbea
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-23 13:35:31 -0600

    feat: service to login created

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { AuthApiService } from './auth-api.service';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 export class LoginComponent {
+
+  username = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required]
+  });
+
+  password = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(6)]
+  });
+
+  constructor(private apiService: AuthApiService) {
+
+  }
+
+  onSubmit() {
+    const credentials = { username: this.username.value, password: this.password.value }
+    this.apiService.login(credentials.username, credentials.password)
+  }
 
 }
