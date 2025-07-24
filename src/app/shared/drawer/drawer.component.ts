553d64f74c4aa924769890f4fec5f2be7e8b00d9
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-24 00:06:38 -0600

    feat: drawer fixed

--- src/app/shared/drawer/drawer.component.ts
+++ src/app/shared/drawer/drawer.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-drawer-component',
-  imports: [DrawerModule, ButtonModule, AvatarModule],
+  imports: [DrawerModule, ButtonModule, AvatarModule, NgClass],
   standalone: true,
   templateUrl: './drawer.component.html',
   styleUrl: './drawer.component.css'
@@ -17,6 +18,11 @@ export class DrawerComponent {
 
   close() {
     this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+
+  open() {
+    this.visible = true;
     this.visibleChange.emit(this.visible);
   }
 }
