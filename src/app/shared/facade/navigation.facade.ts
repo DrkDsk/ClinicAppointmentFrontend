6fec5176253636ee8e3a1625b92779ffda74f44e
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-24 17:14:59 -0600

    feat: navigation facade created

---
+++ src/app/shared/facade/navigation.facade.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Injectable({ providedIn: 'root' })
+export class NavigationFacade {
+    constructor(private router: Router) { }
+
+    navigate(path: string) {
+        this.router.navigate([`/${path}`])
+    }
+}
