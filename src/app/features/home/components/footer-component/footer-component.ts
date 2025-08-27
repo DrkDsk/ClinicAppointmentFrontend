02a1163376ed645b5550e6acd044769ff0c2e6ef
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-27 17:13:44 -0600

    feat: footer implemented

---
+++ src/app/features/home/components/footer-component/footer-component.ts
@@ -0,0 +1,14 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-component',
+  imports: [NgStyle],
+  templateUrl: './footer-component.html',
+  styleUrl: './footer-component.css'
+})
+export class FooterComponent {
+
+  background = "assets/images/footer.jpg"
+
+}
