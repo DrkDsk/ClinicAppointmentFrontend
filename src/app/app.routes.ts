01d44abc318b933e9096ab0a75c0c656aec09ec0
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-08-28 13:58:04 -0600

    fix: drawer fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
+import { NoDrawerLayoutComponent } from './core/shared/presentation/no-drawer-layout/no-drawer-layout.component';
+import { DrawerComponent } from './core/shared/presentation/drawer/drawer.component';
 
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./core/shared/presentation/main/main.component').then(m => m.MainComponent),
+        component: NoDrawerLayoutComponent,
         children: [
             {
                 path: '',
                 loadComponent: () => import('./features/home/presentation/home-component').then(m => m.HomeComponent)
-            },
+            }
+        ]
+    },
+    {
+        path: '',
+        component: DrawerComponent,
+        children: [
             {
                 path: 'dashboard',
                 loadComponent: () => import('./features/dashboard/presentation/dashboard-component').then(m => m.DashboardComponent)
---
+++ src/app/core/shared/presentation/no-drawer-layout/no-drawer-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-no-drawer-layout.component',
+  imports: [RouterOutlet],
+  templateUrl: './no-drawer-layout.component.html',
+  styleUrl: './no-drawer-layout.component.css'
+})
+export class NoDrawerLayoutComponent { }
