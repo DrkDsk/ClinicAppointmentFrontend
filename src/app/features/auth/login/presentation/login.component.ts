8425464bd7fa9b70161cc34a55dd0c3508069e7d
Alfredo Palacios <joseaph.1998@gmail.com>
2025-10-23 17:05:02 -0600

    feat: lazy loading img

--- src/app/features/auth/login/presentation/login.component.ts
+++ src/app/features/auth/login/presentation/login.component.ts
@@ -16,7 +16,7 @@ import {AuthRepositoryImpl} from '../data/repositories/auth.repository.impl';
 import {AuthRepository} from '../domain/repositories/auth.repository';
 import {AUTH_API_SERVICE} from '../data/services/auth_api.service.injection.token';
 import {AUTH_REPOSITORY} from '../domain/repositories/auth.repository.injection.token';
-import {NgClass} from '@angular/common';
+import {NgClass, NgOptimizedImage} from '@angular/common';
 import {RoleService} from '../../../../core/shared/data/services/role/role.service';
 import {AppPaths} from '../../../../core/constants/path.constants';
 
@@ -35,7 +35,8 @@ import {AppPaths} from '../../../../core/constants/path.constants';
     InputTextModule,
     SelectModule,
     InputNumberModule,
-    NgClass
+    NgClass,
+    NgOptimizedImage
   ],
   providers: [
     {provide: AUTH_API_SERVICE, useClass: AuthApiServiceImpl},
@@ -59,7 +60,7 @@ export class LoginComponent {
       validators: [Validators.required, Validators.minLength(6)]
     })
   });
-
+  
   constructor(@Inject(AUTH_REPOSITORY) private loginRepository: AuthRepository,
               private tokenService: TokenService,
               private roleService: RoleService,
