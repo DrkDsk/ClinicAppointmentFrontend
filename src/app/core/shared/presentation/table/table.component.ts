16b86687f1162ad541e89d02c65a82dcef70b26c
Alfredo Palacios <joseaph.1998@gmail.com>
2025-11-06 17:11:08 -0600

    feat: style modified

--- src/app/core/shared/presentation/table/table.component.ts
+++ src/app/core/shared/presentation/table/table.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {PaginatorComponent} from '../paginator/paginator.component';
 
 export interface TableColumn<T = any> {
   header: string;
@@ -8,14 +9,45 @@ export interface TableColumn<T = any> {
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [
+    PaginatorComponent
+  ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent<T> {
 
+  @Input() enablePagination: boolean = true;
   @Input({required: true}) list: T[] = [];
   @Input({required: true}) columns: TableColumn<T>[] = [];
+
+  @Output() goFirstPage = new EventEmitter<void>();
+  @Output() prev = new EventEmitter<void>();
+  @Output() next = new EventEmitter<void>();
+  @Output() loadDoctors = new EventEmitter<number>();
+
+  @Input({required: true}) public from: number = 0;
+  @Input({required: true}) public to: number = 0;
+  @Input({required: true}) public currentPage: number = 0;
+  @Input({required: true}) public lastPage: number = 0;
+  @Input({required: true}) public totalRecords: number = 0;
+  @Input({required: true}) public listOfPages: number[] = [];
+
+  handleGoFirstPage() {
+    this.goFirstPage.emit();
+  }
+
+  handleNextPage() {
+    this.next.emit();
+  }
+
+  handlePrevPage() {
+    this.prev.emit();
+  }
+
+  handleLoadDoctors(page: number) {
+    this.loadDoctors.emit(page);
+  }
 
   getCellValue(row: any, col: any): any {
     if (col.cell) return col.cell(row);
