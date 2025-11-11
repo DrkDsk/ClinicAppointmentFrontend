3e3045643351b66ebe0ce0f79f95230b090eca8a
Alfredo Palacios <joseaph.1998@gmail.com>
2025-11-11 14:37:43 -0600

    feat: template created

--- src/app/features/appointments/presentation/create/create-appointment.component.ts
+++ src/app/features/appointments/presentation/create/create-appointment.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-import {Step, StepList, Stepper} from 'primeng/stepper';
 import {DatePicker} from 'primeng/datepicker';
 import {FloatLabel} from 'primeng/floatlabel';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {InputText} from 'primeng/inputtext';
 
 @Component({
   selector: 'app-create-appointment.component',
   imports: [
-    Stepper,
-    Step,
-    StepList,
     DatePicker,
     FloatLabel,
     FormsModule,
-    InputText,
     ReactiveFormsModule
   ],
   templateUrl: './create-appointment.component.html',
